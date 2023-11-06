6069d4631e5b6c663fd04bd0c478ea71d59099e6
mmzeynalli <miradil.zeynalli@gmail.com>
2023-11-06 23:59:38 +0100

    Added more content

--- cache_fields/models.py
+++ cache_fields/models.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from dirtyfields import DirtyFieldsMixin
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save, pre_save
@@ -12,11 +16,15 @@ class Status(models.Model):
         return self.name
 
 
-class Package(models.Model):
+class Package(DirtyFieldsMixin, models.Model):  # Solution 4
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     shipment_cost = models.DecimalField(max_digits=6, decimal_places=2)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.cached_status_id = self.status_id  # Solution 3
 
     def __str__(self) -> str:
         return f"{self.id}: {self.status}"
@@ -33,11 +41,13 @@ class PackageStatusHistory(models.Model):
 
 @receiver(pre_save, sender=Package)
 def register_status_change(sender, instance: Package, **kwargs):
-    old_status_id = instance.id and Package.objects.get(id=instance.id).status_id
+    old_status_id = instance.cached_status_id  # Solution 3
+    # print(instance.get_dirty_fields())
+    # old_status = instance.get_dirty_fields().get("status", None)  # Solution 4
+    # old_status_id = old_status and old_status.id
     
     if instance.status_id != old_status_id:
         # There is a status change
         PackageStatusHistory.objects.create(package_id=instance.id,
                                             from_status_id=old_status_id,
                                             to_status_id=instance.status_id)
-
